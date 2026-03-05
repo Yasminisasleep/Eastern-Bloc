39e5fca7c1c5c6989d4a42648485c26173ba1246
Yasminisasleep <Benaliyasmine904@gmail.com>
2026-03-05 23:07:30 +0100

    feat: spring boot + health endpoint

---
+++ backend/src/main/java/com/kulto/KultoApplication.java
@@ -0,0 +1,12 @@
+package com.kulto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KultoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KultoApplication.class, args);
+    }
+}
